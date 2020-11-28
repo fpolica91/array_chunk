9a968b653333eda1c09c476a36fc7b78528b183e
Fabricio <fabriciopolicarpo0@gmail.com>
2020-11-28 21:33:48 +0000

    double loop

--- problems/steps.js
+++ problems/steps.js
@@ -6,7 +6,23 @@ function steps(n){
 }
 
 
+function bySteps(n){
+  for(let row = 0; row < n; row++){
+    // create empty string for each iteration, needs to be empty each time
+    //we reiterate over row.
+    let step ="";
+    for(let column = 0; column < n; column++){
+      if(column <= row){
+        step += "#"
+      }else{
+        step += " "
+      }
+    }
+    console.log(step)
+  }
+}
 
 
 
-steps(4)
+
+bySteps(4)
